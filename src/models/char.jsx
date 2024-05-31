b112f87a275ada900a9b030064e89cff949614fa
Preisler25 <preisler.andras@gmail.com>
2024-05-31 14:25:42 +0200

    feat: main

---
+++ src/models/char.jsx
@@ -0,0 +1,10 @@
+class Char {
+    constructor(id, name, role, strength, health, img) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+        this.strength = strength;
+        this.health = health;
+        this.img = img;
+    }
+}
