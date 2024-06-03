72beabe60d5c218f60ae155c658b9c36d6311e15
Preisler <preisler.andras@gmail.com>
2024-06-03 13:33:51 +0200

    feat: scroll-bar

--- src/components/card_layout.jsx
+++ src/components/card_layout.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function CardLayout({ children }) {
-    console.log(children);
-    return (
-        <div className="h-4/5 grid grid-cols-1 md:grid-cols-3 gap-4 w-full p-4 overflow-scroll rounded-lg">
-        {children}
-        </div>
-    );
+  console.log(children);
+  return (
+    <div className="h-4/5 grid grid-cols-1 md:grid-cols-3 gap-4 w-full p-4 overflow-scroll rounded-lg overflow-x-hidden">
+      {children}
+    </div>
+  );
 }
--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,27 +1,31 @@
 // Display.js
-import React, { useContext, useState } from 'react';
-import { CharListContext } from '../contexts/char_list_context';
-import Card from './card';
-import Editor from './editor';
-import Navbar from './navbar';
-import CardLayout from './card_layout';
+import React, { useContext, useState } from "react";
+import { CharListContext } from "../contexts/char_list_context";
+import Card from "./card";
+import Editor from "./editor";
+import Navbar from "./navbar";
+import CardLayout from "./card_layout";
 
 export default function Display() {
-    const { charList } = useContext(CharListContext);
-    const [mode, setMode] = useState('edit');
-    const [selectedChar, setSelectedChar] = useState(charList[0]);
+  const { charList } = useContext(CharListContext);
+  const [mode, setMode] = useState("edit");
+  const [selectedChar, setSelectedChar] = useState(charList[0]);
 
-    return (
-        <div className='border-2 border-red-800 rounded-lg'>
-            <Navbar mode={mode} setMode={setMode} />
-            <div className="p-4 h-screen bg-slate-800 flex">
-                <Editor mode={mode} selectedChar={selectedChar} setSelectedChar={setSelectedChar} />
-                <CardLayout>
-                    {charList.map(char => (
-                        <Card key={char.id} char={char} />
-                    ))}
-                </CardLayout>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar mode={mode} setMode={setMode} />
+      <div className="p-4 h-screen bg-slate-800 flex">
+        <Editor
+          mode={mode}
+          selectedChar={selectedChar}
+          setSelectedChar={setSelectedChar}
+        />
+        <CardLayout>
+          {charList.map((char) => (
+            <Card key={char.id} char={char} />
+          ))}
+        </CardLayout>
+      </div>
+    </div>
+  );
 }
